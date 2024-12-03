35a1f767499e4216c0209346d54039b69fb3bf65
Attila Horvath <attila.horvath@cashanalytics.com>
2024-12-03 01:26:51 +0000

    Add solution for both parts of day 2

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -1,55 +1,79 @@
+use std::cmp::Ordering;
 use std::error::Error;
 use std::io::{self, BufRead, Read};
-use std::cmp::Ordering;
 
-pub fn safe_reports(input: impl Read) -> Result<usize, Box<dyn Error>> {
+pub fn safe_reports(input: impl Read, allow_exception: bool) -> Result<usize, Box<dyn Error>> {
     let mut count = 0;
 
-    'reports: for report in io::BufReader::new(input).lines() {
-        let report = report?;
+    for report in io::BufReader::new(input).lines() {
+        let report = report?
+            .split_ascii_whitespace()
+            .map(|level| level.parse::<i32>().unwrap_or_default())
+            .collect::<Vec<_>>();
 
-        let mut prev_level: Option<i32> = None;
-        let mut order = Ordering::Equal;
+        if check_report(&report) {
+            count += 1;
+        } else if allow_exception {
+            for i in 0..report.len() {
+                let mut report_with_exception = report.clone();
+                report_with_exception.remove(i);
 
-        for level in report.split_ascii_whitespace() {
-            let level = level.parse::<i32>().unwrap_or_default();
-
-            if let Some(prev) = prev_level {
-                let curr_order = level.cmp(&prev);
-                let difference = (level - prev).abs();
-
-                if order == Ordering::Equal {
-                    order = curr_order;
-                }
-
-                if curr_order != order || difference < 1 || difference > 3 {
-                    continue 'reports;
+                if check_report(&report_with_exception) {
+                    count += 1;
+                    break;
                 }
             }
-
-            prev_level = Some(level);
         }
-
-        count += 1;
     }
 
     Ok(count)
+}
+
+fn check_report(report: &[i32]) -> bool {
+    let mut prev_level: Option<i32> = None;
+    let mut order = Ordering::Equal;
+
+    for &level in report {
+        if let Some(prev) = prev_level {
+            let curr_order = level.cmp(&prev);
+            let difference = (level - prev).abs();
+
+            if order == Ordering::Equal {
+                order = curr_order;
+            }
+
+            if curr_order != order || difference < 1 || difference > 3 {
+                return false;
+            }
+        }
+
+        prev_level = Some(level);
+    }
+
+    true
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn count_safe_reports() -> Result<(), Box<dyn Error>> {
-        let input = "7 6 4 2 1
+    const INPUT: &str = "7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9";
 
-        assert_eq!(2, safe_reports(input.as_bytes())?);
+    #[test]
+    fn count_safe_reports() -> Result<(), Box<dyn Error>> {
+        assert_eq!(2, safe_reports(INPUT.as_bytes(), false)?);
+
+        Ok(())
+    }
+
+    #[test]
+    fn count_safe_reports_with_exceptions() -> Result<(), Box<dyn Error>> {
+        assert_eq!(4, safe_reports(INPUT.as_bytes(), true)?);
 
         Ok(())
     }
--- day02/src/main.rs
+++ day02/src/main.rs
@@ -4,7 +4,15 @@ use std::fs::File;
 use day02::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Safe reports: {}", safe_reports(File::open("input.txt")?)?);
+    println!(
+        "Safe reports: {}",
+        safe_reports(File::open("input.txt")?, false)?
+    );
+
+    println!(
+        "Safe reports with exceptions: {}",
+        safe_reports(File::open("input.txt")?, true)?
+    );
 
     Ok(())
 }
